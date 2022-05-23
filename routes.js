bc6c8b814d2b28d05d977112ebd37499a1a2a3d4
Eduardo Benitez <eduardombc96@gmail.com>
2022-05-23 14:51:28 -0500

    Customized dev server bootup script

---
+++ routes.js
@@ -0,0 +1,4 @@
+// Sets up custom urls that require a dynamic slug.
+const routes = require('next-routes')();
+
+module.exports = routes;
---
+++ server.js
@@ -0,0 +1,17 @@
+// https://github.com/fridays/next-routes
+// Allows manual boot of server by EXPLICITLY telling it to use
+// the routes.js file in order to be able to use dynamic slugs
+// on our urls.
+
+const next = require('next');
+const routes = require('./routes');
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
+const handler = routes.getRequestHandler(app);
+
+const { createServer } = require('http')
+app.prepare().then(() => {
+    createServer(handler).listen(3000, (err) => {
+        if (err) throw err;
+        console.log('Ready on localhost:3000');
+    })
+});
