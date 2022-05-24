1cbd1d869df118967f1f4cbb0184a74b6c56e8fb
Eduardo Benitez <eduardombc96@gmail.com>
2022-05-24 10:11:48 -0500

    Created routes for individual campaigns using wildcard

--- routes.js
+++ routes.js
@@ -1,4 +1,11 @@
 // Sets up custom urls that require a dynamic slug.
 const routes = require('next-routes')();
 
+
+// The route using a wildcard would override the previous one if it was
+// added before.
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    .add('/campaigns/:campaignAddress', '/campaigns/show');
+
 module.exports = routes;
