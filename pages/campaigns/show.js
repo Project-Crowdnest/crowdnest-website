67b1608886a6d0923737461f9a58aeca25e8332b
Eduardo Benitez <eduardombc96@gmail.com>
2022-05-24 14:06:22 -0500

    CampaignShow's GetInitialProps

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 
 class CampaignShow extends Component {
+    // This function its called with a props object
+    // of its own. This will not end up inside this component's
+    // instance.
+    static async getInitialProps(props) {
+        console.log(props.query.campaignAddress);
+
+        return {};
+    }
+
     render () {
         return (
             <Layout>
